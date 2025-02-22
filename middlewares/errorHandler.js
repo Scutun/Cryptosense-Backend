d8b16bf370919215700c32fbc763789b0dc67750
Alex <kevga123456789@gmail.com>
2025-02-22 15:31:35 +0300

    fix: errorHandler messaga

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,9 +1,6 @@
 function errorHandler(err, req, res, next) {
     res.status(err.status || 500).json({
-        error: {
-            code: err.status || 500,
-            message: err.message || 'Internal server error',
-        },
+        message: err.message || 'Внутренняя ошибка сервера',
     })
 }
 
