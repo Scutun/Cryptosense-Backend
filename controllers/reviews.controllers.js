efc2b8bedb4fb2a1306a23da6b1b67348bf6e74c
Alex <kevga123456789@gmail.com>
2025-02-24 18:06:46 +0300

    fix: to merge

--- controllers/reviews.controllers.js
+++ controllers/reviews.controllers.js
@@ -2,6 +2,23 @@ const tokenUtils = require('../utils/tokens.utils')
 const reviewsService = require('../services/reviews.services')
 
 class ReviewsController {
+    async getReviewByCourseId(id) {
+        try {
+            if (id.length === 0) {
+                throw { status: 400, message: 'Id курса не предоставлен' }
+            }
+
+            const info = await reviewsModel.getReviewByCourseId(id)
+
+            if (!info[0]) {
+                throw { status: 404, message: 'Курс не найден' }
+            }
+            return info
+        } catch (error) {
+            throw error
+        }
+    }
+
     async createReview(req, res, next) {
         try {
             const user = tokenUtils.getIdFromToken(req)
@@ -41,24 +58,6 @@ class ReviewsController {
             next(error)
         }
     }
-
-    async getReviewByCourseId(id) {
-        try {
-            if (id.length === 0) {
-                throw { status: 400, message: 'Id курса не предоставлен' }
-            }
-
-            const info = await reviewsModel.getReviewByCourseId(id)
-
-            if (!info[0]) {
-                throw { status: 404, message: 'Курс не найден' }
-            }
-            return info
-        } catch (error) {
-            throw error
-        }
-    }
-
 }
 
 module.exports = new ReviewsController()
