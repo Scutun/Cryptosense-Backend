fce9caa865f57c74f5d6a07b63ca1b4b511709e8
Alex <kevga123456789@gmail.com>
2025-03-16 10:45:34 +0300

    fix: removed emogi

--- config/doc.js
+++ config/doc.js
@@ -4,14 +4,14 @@ const path = require('path');
 const yaml = require('js-yaml');
 const glob = require('glob');
 
-// Пути
+
 const docsPath = path.join(__dirname, '../docs'); // Путь к папке docs
 const outputPath = path.join(docsPath, 'main.swagger.yaml'); // Путь к объединённому файлу
 
 // Находим все .swagger.yaml файлы в папке docs (исключаем main.swagger.yaml)
 const swaggerFiles = glob.sync(path.join(docsPath, '*.swagger.yaml')).filter(file => !file.includes('main.swagger.yaml'));
 
-console.log('✅ Найдены файлы для объединения:', swaggerFiles);
+console.log('Найдены файлы для объединения:', swaggerFiles);
 
 // Основная структура OpenAPI
 const mergedData = {
@@ -31,12 +31,12 @@ swaggerFiles.forEach(file => {
     if (content.paths) {
       Object.assign(mergedData.paths, content.paths);
     }
-    console.log(`📄 Объединён: ${file}`);
+    console.log(`Объединён: ${file}`);
   } catch (error) {
-    console.error(`❌ Ошибка при обработке файла: ${file}`, error);
+    console.error(`Ошибка при обработке файла: ${file}`, error);
   }
 });
 
 // Сохраняем объединённый файл
 fs.writeFileSync(outputPath, yaml.dump(mergedData), 'utf8');
-console.log(`🎉 Файл успешно объединён: ${outputPath}`);
+console.log(`Файл успешно объединён: ${outputPath}`);
