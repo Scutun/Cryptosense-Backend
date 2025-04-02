86e631201adc49e38f68e3d9a788674c270274a9
Scutun <evplotnikov04@ya.ru>
2025-04-02 18:09:44 +0300

    fix (routes): changed middleware token checking

--- routes/reviews.routes.js
+++ routes/reviews.routes.js
@@ -4,12 +4,14 @@ const router = new Router()
 const reviewsController = require('../controllers/reviews.controllers')
 const checkToken = require('../middlewares/checkToken')
 
-router.post('/v1/reviews/new', checkToken, reviewsController.createReview)
-
 router.get('/v1/reviews/course/:id', reviewsController.getReviewByCourseId)
 
-router.put('/v1/reviews/redact', checkToken, reviewsController.changeReview)
+router.use('/v1/reviews', checkToken)
 
-router.delete('/v1/reviews/:id', checkToken, reviewsController.deleteReview)
+router.post('/v1/reviews/new', reviewsController.createReview)
+
+router.put('/v1/reviews/redact', reviewsController.changeReview)
+
+router.delete('/v1/reviews/:id', reviewsController.deleteReview)
 
 module.exports = router
