862ec07d075c911ad5dd71c7c5d22def33c30452
Scutun <evplotnikov04@ya.ru>
2025-04-04 12:42:01 +0300

    fix (tests): changed checking of outputs

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,36 +2,37 @@ const request = require('supertest')
 const { describe, it, expect } = require('@jest/globals')
 const app = require('../app')
 
+const token =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzQzNzU5MjUxLCJleHAiOjE3NDM3NjY0NTF9.qSMESBSJnvXZBkZn-xSl2Of0O7_QO1dXawnr9Vgm3WQ'
+
 describe('Тестирование API /v1/users/me', () => {
     it('Должен вернуть информацию о пользователе', async () => {
         const response = await request(app)
             .get('/api/v1/users/me')
-            .set(
-                'Authorization',
-                'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzQzNjY1NjU5LCJleHAiOjE3NDM2NzI4NTl9.eI9D5PJZJJZs4be2UG05nXXTVA5SGGq85lrvYyHDd_8',
-            )
+            .set('Authorization', `Bearer ${token}`)
             .set('Content-Type', 'application/json')
 
         expect(response.statusCode).toBe(200)
-        expect(response.body).toHaveProperty('email')
-        expect(response.body).toHaveProperty('name')
-        expect(response.body).toHaveProperty('surname')
-        expect(response.body).toHaveProperty('nickname')
-        expect(response.body).toHaveProperty('registrationdate')
-        expect(response.body).toHaveProperty('reputation')
-        expect(response.body).toHaveProperty('photo')
-        expect(response.body).toHaveProperty('achievements')
-        expect(response.body).toHaveProperty('description')
-        expect(response.body).toHaveProperty('isauthor')
+        expect(response.body).toEqual(
+            expect.objectContaining({
+                email: expect.any(String),
+                name: expect.any(String),
+                surname: expect.any(String),
+                nickname: expect.any(String),
+                registrationdate: expect.any(String),
+                reputation: expect.any(String),
+                photo: expect.any(String),
+                achievements: expect.any(Array),
+                description: expect.any(String),
+                isauthor: expect.any(Boolean),
+            }),
+        )
     })
 
     it('Должен вернуть ошибку при неверном токене', async () => {
         const response = await request(app)
             .get('/api/v1/users/me')
-            .set(
-                'Authorization',
-                'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzQzNjY1NjU5LCJleHAiOjE3NDM2NzI4NTl9q85lrvYyHDd_8',
-            )
+            .set('Authorization', `Bearer kansldn`)
             .set('Content-Type', 'application/json')
 
         expect(response.statusCode).toBe(403)
@@ -42,10 +43,7 @@ describe('Тестирование API /v1/users/new/info', () => {
     it('Должен вернуть токен при правильных данных', async () => {
         const response = await request(app)
             .put('/api/v1/users/new/info')
-            .set(
-                'Authorization',
-                'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzQzNjY1NjU5LCJleHAiOjE3NDM2NzI4NTl9.eI9D5PJZJJZs4be2UG05nXXTVA5SGGq85lrvYyHDd_8',
-            )
+            .set('Authorization', `Bearer ${token}`)
             .set('Content-Type', 'application/json')
             .send({
                 name: 'Алексей',
@@ -59,15 +57,20 @@ describe('Тестирование API /v1/users/new/info', () => {
         expect(response.body).toHaveProperty('surname')
         expect(response.body).toHaveProperty('nickname')
         expect(response.body).toHaveProperty('photo')
+        expect(response.body).toEqual(
+            expect.objectContaining({
+                name: expect.any(String),
+                surname: expect.any(String),
+                nickname: expect.any(String),
+                photo: expect.any(String),
+            }),
+        )
     })
 
     it('Должен вернуть ошибку при неверных данных', async () => {
         const response = await request(app)
             .put('/api/v1/users/new/info')
-            .set(
-                'Authorization',
-                'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzQzNjY1NjU5LCJleHAiOjE3NDM2NzI4NTl9.eI9D5PJZJJZs4be2UG05nXXTVA5SGGq85lrvYyHDd_8',
-            )
+            .set('Authorization', `Bearer ${token}`)
             .set('Content-Type', 'application/json')
             .send({
                 name: 'Алексей',
