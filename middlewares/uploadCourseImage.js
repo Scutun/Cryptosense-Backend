c5d963646f879eebd63a60755fac4e7726b50bd7
Scutun <evplotnikov04@ya.ru>
2025-05-13 19:54:18 +0300

    fix(courses): changed directory for photo

--- middlewares/uploadCourseImage.js
+++ middlewares/uploadCourseImage.js
@@ -2,7 +2,7 @@ const multer = require('multer')
 const path = require('path')
 const fs = require('fs')
 
-const uploadDir = 'uploads/courses'
+const uploadDir = 'uploads/course'
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true })
 }
