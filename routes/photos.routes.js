2cbc371183c793df4aa69732cc907336529c1d34
Scutun <evplotnikov04@ya.ru>
2025-05-20 21:14:15 +0300

    fix(courses): fixed getting of courses info

--- routes/photos.routes.js
+++ routes/photos.routes.js
@@ -2,15 +2,15 @@ const Router = require('express')
 const router = new Router()
 
 const photosUtils = require('../utils/photos.utils')
-const checkToken = require('../middlewares/checkToken')
 const expressStatic = require('express').static
-
 const { serveCoursePhoto, courseUploadsPath } = require('../utils/photos.utils')
+
+const checkToken = require('../middlewares/checkToken')
+
+router.get('/v1/profiles/avatars/all', checkToken, photosUtils.getPhotos)
 
 router.use('/v1/courses/photo/url', serveCoursePhoto)
 
 router.use('/v1/courses/photo/url', expressStatic(courseUploadsPath))
-
-router.get('/v1/profiles/avatars/all', checkToken, photosUtils.getPhotos)
 
 module.exports = router
